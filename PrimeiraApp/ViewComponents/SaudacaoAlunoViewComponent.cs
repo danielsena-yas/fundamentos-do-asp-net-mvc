22ab981f0c4b2a1ae7af5ebc4925e34789fdf99b
Daniel Sena <danielsena.yas@gmail.com>
2025-04-27 17:32:52 -0300

    05.05 - Apresentando os View Components

---
+++ PrimeiraApp/ViewComponents/SaudacaoAlunoViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimeiraApp.Models;
+
+namespace PrimeiraApp.ViewComponents
+{
+    public class SaudacaoAlunoViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            // Pega o aluno da base de dados ou pega o nome do aluno logado
+            var aluno = new Aluno() { Nome = "Eduardo" };
+            return View(aluno);
+        }
+    }
+}
