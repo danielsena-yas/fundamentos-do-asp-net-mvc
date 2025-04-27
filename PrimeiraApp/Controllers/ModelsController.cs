579b0642a9da178bc0884eb448c21cdeceb9e617
Daniel Sena <danielsena.yas@gmail.com>
2025-04-27 16:22:34 -0300

    04.04 - Validação de Modelos

---
+++ PrimeiraApp/Controllers/ModelsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimeiraApp.Models;
+
+namespace PrimeiraApp.Controllers
+{
+    public class ModelsController : Controller
+    {
+        public IActionResult Index()
+        {
+            var aluno = new Aluno()
+            {
+                Nome = "A",
+                Email = "Eduardo",
+                EmailConfirmacao = "eduardo.com"
+            };
+
+            if (TryValidateModel(aluno))
+            {
+                return View(aluno);
+            }
+
+            var ms = ModelState;
+            var erros = ModelState
+                .Select(x => x.Value?.Errors)
+                .Where(y => y.Count > 0)
+                .ToList();
+            erros.ForEach(r => Console.WriteLine(r?.First().ErrorMessage));
+
+            return View();
+        }
+    }
+}
